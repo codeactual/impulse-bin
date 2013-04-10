8b7e33f5733c6700ed291f049daf0e91f5629144
David Smith <codeactual@gmail.com>
2013-04-09 23:52:04 -0700

    feat(api): Update module dep injection, execution
    - Inject deps via context
    - Support running multiple modules

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 # cmdr-input
 
-commander.js input handling class
+Modules for commander.js
 
 [![Build Status](https://travis-ci.org/codeactual/cmdr-input.png)](https://travis-ci.org/codeactual/cmdr-input)
 
+## Goals
+
+* CLI scripts that are easier to test without running the executable.
+* Reduce boilerplate via utility functions and injected dependencies like ShellJS.
+
 ## Example
 
+```js
+// ./bin/cli
+var ci = require('cmdr-input').create();
+ci
+  .set('nativeRequire', require)
+  .push(require('./lib/cli'))
+  .run(commander);
 
+// ./lib/cli/index.js
+module.exports = function() {
+  this.exitOnMissingOption(this.input, ['config']);
+
+  // ...
+
+  this.promise.resolve();
+};
+```
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * commander.js input handling class
+ * Modules for commander.js
  *
  * Licensed under MIT.
  * Copyright (c) 2013 David Smith <https://github.com/codeactual/>
@@ -9,40 +9,101 @@
 'use strict';
 
 module.exports = {
+  create: create,
   CmdrInput: CmdrInput,
-  require: require // Allow tests to use component-land require.
+  require: require, // Allow tests to use component-land require.
+  mixinCmdrInput: mixinCmdrInput,
+  mixinHelpers: mixinHelpers
 };
 
 var configurable = require('configurable.js');
 
+function create() {
+  return new CmdrInput();
+}
+
 function CmdrInput() {
   this.settings = {
-    input: {},
-    nativeRequire: {}
+    nativeRequire: {},
+    done: noOp
   };
 }
 
 configurable(CmdrInput.prototype);
 
-CmdrInput.prototype.run = function(cb) {
-  var rsvp = this.get('nativeRequire')('rsvp');
-  var promise = new rsvp.Promise();
-  cb.call(this, promise);
-  return promise;
+/**
+ * Add a step to the CLI run.
+ *
+ * Each step typically defined as a module whose export is a function.
+ * The function accepts a single argument, defined in run(), and is responsible
+ * for resolving an injected promise.
+ *
+ * @param {function} cb
+ */
+CmdrInput.prototype.push = function(cb) {
+  this.steps.push(cb);
 };
 
-CmdrInput.prototype.exit = function(msg, code) {
+CmdrInput.prototype.run = function(commander) {
+  var extend = require('extend');
+  var nativeRequire = this.get('nativeRequire');
+  var rsvp = nativeRequire('rsvp');
+  var promises = [];
+  this.steps.forEach(function(cb) {
+    var promise = rsvp.promise();
+    var context = {
+      fs: nativeRequire('fs'),
+      input: this.input,
+      promise: promise,
+      rsvp: rsvp,
+      shelljs: require('outer-shelljs').create(),
+      util: nativeRequire('util')
+    };
+    extend(context, helpers);
+    promises.push(promises);
+    cb.call(context);
+  });
+  rsvp.all(promises).then(this.get('done'));
+};
+
+var helpers = {};
+
+helpers.requireNode = function(id) {
+  return this.get('nativeRequire')(id);
+};
+
+helpers.requireComponent = function(id) {
+  return require(id);
+};
+
+helpers.exit = function(msg, code) {
   this.get('nativeRequire')('util').error(msg);
   process.exit(typeof code === 'undefined' ? 1 : code);
 };
 
-CmdrInput.prototype.exitOnShellError = function(res) {
+helpers.exitOnShelljsError = function(res) {
   if (res.code !== 0) { this.exit(res.output, res.code); }
 };
 
-CmdrInput.prototype.exitOnMissingParam = function(params) {
+helpers.exitOnMissingOption = function(input, options, code) {
   var self = this;
-  params.forEach(function(param) {
-    if (!self.get('input')[param]) { self.exit('--' + param + ' required'); }
+  options.forEach(function(param) {
+    if (!input[param]) { self.exit('--' + param + ' required', code); }
   });
 };
+
+/**
+ * Mix the given function set into CmdrInput's prototype.
+ *
+ * @param {object} ext
+ */
+function mixinCmdrInput(ext) { require('extend')(CmdrInput.prototype, ext); }
+
+/**
+ * Mix the given function set into the helpers object.
+ *
+ * @param {object} ext
+ */
+function mixinHelpers(ext) { require('extend')(helpers, ext); }
+
+function noOp() {}
