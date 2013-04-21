0b3e6c9e80caaede2af3f15f156f91d2f64daf68
David Smith <codeactual@gmail.com>
2013-04-21 00:21:16 -0700

    test(lib): Add plan

--- test/lib/impulse-bin/index.js
+++ test/lib/impulse-bin/index.js
@@ -9,15 +9,110 @@ var impulseBin = require('../../..');
 
 require('sinon-doublist')(sinon, 'mocha');
 
-describe('impulse-bin', function() {
-  describe('ImpulseBin', function() {
-    beforeEach(function() {
-      this.input = {};
-      this.ci = impulseBin.create();
+describe('ImpulseBin', function() {
+  beforeEach(function() {
+    this.ib = impulseBin.create();
+  });
+
+  describe('#run', function() {
+    it.skip('should store ref to adapter', function() {
     });
 
-    it('should do something', function() {
-      console.log('\x1B[33m<---------- INCOMPLETE');
+    it.skip('should store ref to provider', function() {
+    });
+
+    it.skip('should extract options with adapter', function() {
+    });
+
+    it.skip('should extract args with adapter', function() {
+    });
+
+    it.skip('should optionally silence loggers', function() {
+    });
+
+    it.skip('should init stdout logger', function() {
+    });
+
+    it.skip('should init stderr logger', function() {
+    });
+
+    it.skip('should init verbose logger', function() {
+    });
+
+    describe('injected context', function() {
+      it.skip('should include extracted args', function() {
+      });
+
+      it.skip('should include child_process', function() {
+      });
+
+      it.skip('should include cli-color', function() {
+      });
+
+      it.skip('should include fs', function() {
+      });
+
+      it.skip('should include extracted options', function() {
+      });
+
+      it.skip('should include provider', function() {
+      });
+
+      it.skip('should include OuterShelljs', function() {
+      });
+
+      it.skip('should include util', function() {
+      });
+
+      it.skip('should include #createVerbose mixin', function() {
+      });
+
+      it.skip('should include #exit mixin', function() {
+      });
+
+      it.skip('should include #exitOnMissingOption mixin', function() {
+      });
+
+      it.skip('should include #exitOnShelljsErr mixin', function() {
+      });
+    });
+  });
+
+  describe('#createVerbose', function() {
+    it.skip('should use default logger name', function() {
+    });
+
+    it.skip('should use optional logger name', function() {
+    });
+
+    it.skip('should return no-op by default', function() {
+    });
+
+    it.skip('should optionally return logger', function() {
+    });
+  });
+
+  describe('#exit', function() {
+    it.skip('should output message to stderr', function() {
+    });
+
+    it.skip('should exit process with default code', function() {
+    });
+
+    it.skip('should exit process with optional code', function() {
+    });
+  });
+
+  describe('#exitOnMissingOption', function() {
+    it.skip('should use #exit', function() {
+    });
+  });
+
+  describe('#exitOnShelljsErr', function() {
+    it.skip('should do nothing on zero code', function() {
+    });
+
+    it.skip('should use #exit on non-zero code', function() {
     });
   });
 });
