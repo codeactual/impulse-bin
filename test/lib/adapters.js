ff9367d63062b6987f5c9e25ab6a93bc97a97a9f
David Smith <codeactual@gmail.com>
2013-04-22 14:42:21 -0700

    test(adapter): Cover commander.js

---
+++ test/lib/adapters.js
@@ -0,0 +1,46 @@
+/*jshint expr:true*/
+var sinon = require('sinon');
+var chai = require('chai');
+
+var should = chai.should();
+chai.Assertion.includeStack = true;
+chai.use(require('sinon-chai'));
+
+require('sinon-doublist')(sinon, 'mocha');
+
+describe('adapter for', function() {
+  beforeEach(function() {
+    process.argv = [
+      'node',
+      '/path/to/script',
+      '--foo',
+      '1',
+      '--bar',
+      '2',
+      'baz',
+      'bat'
+    ];
+    this.options = {foo: 1, bar: 2};
+    this.args = ['baz', 'bat'];
+  });
+
+  describe('commander.js', function() {
+    beforeEach(function() {
+      var commander = require('commander');
+      commander.option('--foo <#>', '', Number).option('--bar <#>', '', Number);
+      commander.parse(process.argv);
+      this.provider = commander;
+      this.adapter = require('../../lib/adapter/commander');
+    });
+
+    it('should extract args', function() {
+      this.adapter.args(this.provider).should.deep.equal(this.args);
+    });
+
+    it('should extract options', function() {
+      var actual = this.adapter.options(this.provider);
+      actual.foo.should.equal(this.options.foo);
+      actual.bar.should.equal(this.options.bar);
+    });
+  });
+});
