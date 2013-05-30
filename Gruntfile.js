0a59638e6852b5e1a261bf511ea514353b141d72
David Smith <codeactual@gmail.com>
2013-05-30 15:47:35 -0700

    chore(grunt): Migrate to grunt-horde

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,11 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-
-  var mochaShelljsOpt = {stdout: true, stderr: false};
-
-  grunt.initConfig({
-    jshint: {
-      src: {
-        files: {
-          src: ['index.js', 'lib/**/*.js']
-        }
-      },
-      bin: {
-        files: {
-          src: ['bin/*']
-        }
-      },
-      grunt: {
-        files: {
-          src: ['Gruntfile.js']
-        }
-      },
-      tests: {
-        files: {
-          src: ['test/lib/**/*.js']
-        }
-      },
-      json: {
-        files: {
-          src: ['*.json']
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        options: {
-          compress: false,
-          mangle: false,
-          beautify: true
-        },
-        files: {
-          'dist/impulse-bin.js': 'dist/impulse-bin.js'
-        }
-      }
-    },
-    shell: {
-      options: {
-        failOnError: true
-      },
-      build: {
-        command: 'component install --dev && component build --standalone impulseBin --name impulse-bin --out dist --dev'
-      },
-      dist: {
-        command: 'component build --standalone impulseBin --name impulse-bin --out dist'
-      },
-      test_lib: {
-        options: mochaShelljsOpt,
-        command: 'mocha --colors --recursive --reporter spec test/lib'
-      },
-      dox_lib: {
-        command: 'apidox --input lib/impulse-bin/index.js --output docs/ImpulseBin.md'
-      }
-    }
-  });
-
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('dox', ['shell:dox_lib']);
-  grunt.registerTask('build', ['default', 'shell:build']);
-  grunt.registerTask('dist', ['default', 'shell:dist', 'uglify:dist', 'dox']);
-  grunt.registerTask('test', ['build', 'shell:test_lib']);
+  require('grunt-horde')
+    .create(grunt)
+    .demand('projName', 'impulse-bin')
+    .demand('instanceName', 'impulseBin')
+    .demand('klassName', 'ImpulseBin')
+    .loot('node-component-grunt')
+    .attack();
 };
